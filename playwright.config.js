a5b3b934e9aecb7db6370657081ae7974e8c790b
Param Factory <factory@sociobot.in>
2026-08-28 11:50:06 +0000

    test: isolate browser specifications

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,7 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./site/tests",
+  testMatch: "**/*.spec.js",
   outputDir: "./test-results",
   timeout: 30_000,
   fullyParallel: true,
